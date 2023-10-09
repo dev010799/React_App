2c24e1033276aa03372b9a0ea69890fd7e36a7b9
Dev Mishra <dev010799@gmail.com>
2023-10-10 03:51:06 +1200

    Create small project of restraurant

---
+++ restraurant/src/component/Basics/Restraurant.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Restraurant } from "./component/Basics/Restraurant";
+
+const Restraurant = () => {
+  return (
+    <>
+        <h1>Hello Restraurant</h1>
+    </>  
+  );
+};
+
+export default Restraurant;
