cfb31136736a049197aeff7f74cdcbb4e86880b4
Dev Mishra <dev010799@gmail.com>
2023-10-19 02:57:23 +1200

    created Basic navbar using react

---
+++ react_basics/src/App.js
@@ -0,0 +1,27 @@
+// import logo from './logo.svg';
+import './App.css';
+
+let name = "Dev";
+function App() {
+  return (
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+
+    <div className="container">
+      <h1>Hello {name}</h1>
+
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum cumque provident, modi architecto obcaecati officiis ipsa repudiandae aspernatur! Velit voluptates error blanditiis molestias fugiat facere praesentium tempora mollitia provident repudiandae distinctio nisi, neque quas?
+
+    </div>
+    </>
+   
+
+    
+  );
+}
+
+export default App;
