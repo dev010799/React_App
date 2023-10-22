bdc18755bf7691e346bf231ba4939d0cd85157ca
Dev Mishra <dev010799@gmail.com>
2023-10-22 18:03:36 +1200

    Basic responsive navbar using bootstrap and react

--- react_basics/src/App.js
+++ react_basics/src/App.js
@@ -5,23 +5,59 @@ let name = "Dev";
 function App() {
   return (
     <>
-    <nav>
-      <li>Home</li>
-      <li>About</li>
-      <li>Contact</li>
-    </nav>
-
-    <div className="container">
-      <h1>Hello {name}</h1>
-
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum cumque provident, modi architecto obcaecati officiis ipsa repudiandae aspernatur! Velit voluptates error blanditiis molestias fugiat facere praesentium tempora mollitia provident repudiandae distinctio nisi, neque quas?
-
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">Link</a>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="/">Action</a></li>
+            <li><a className="dropdown-item" href="/">Another action</a></li>
+            <li><hr className="dropdown-divider" /></li>
+            <li><a className="dropdown-item" href="/">Something else here</a></li>
+          </ul>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
     </div>
-    </>
-   
-
+  </div>
+</nav>
     
+    </> 
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
