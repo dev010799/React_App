af5dcda0fd9fad2dd145cbe438771021086b4a6d
Dev Mishra <dev010799@gmail.com>
2023-10-31 01:42:44 +1200

    added one lowercase button and add some content on react app

--- react_basics/src/components/TextForm.js
+++ react_basics/src/components/TextForm.js
@@ -1,26 +1,42 @@
 import React, {useState} from 'react'
 
 export default function TextForm(props) {
-    const handleUpClick = () => {
+const handleUpClick = () => {
         console.log("Uppercase was Clicked" + text);
         let newText = text.toUpperCase();
         setText(newText);
     }
+  const handleLoClick = () => {
+         console.log("Lowercase was Clicked" + text);
+         let newText = text.toLowerCase();
+         setText(newText);
 
-    const handleOnChange = (event) => {
+    }
+
+const handleOnChange = (event) => {
         console.log("On change");
         setText(event.target.value);
     }
-    const [text, setText] = useState('Enter text here');
-  return (
+const [text, setText] = useState('Enter text here');
+ return (
+    <>
     <div>
     <h1>{props.heading}</h1>
 
     <div className="mb-3">
     <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
     </div>
-    <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
-  
+    <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+    <button className="btn btn-primary" onClick={handleLoClick}>Convert to Lowercase</button>
     </div>
+
+    <div className="container my-3">
+        <h1>Your text summary</h1>
+        <p>{text.split(" ").length} words and  {text.length} characters</p>
+        <p>{0.008 * text.split(" ").length} Minutes</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+    </div>
+    </>
   )
 }
