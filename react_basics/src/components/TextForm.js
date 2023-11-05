86577325cb7588ed967828f80bd6b7df3bfa3df5
Dev Mishra <dev010799@gmail.com>
2023-11-05 18:43:41 +1200

    added the clear text button

--- react_basics/src/components/TextForm.js
+++ react_basics/src/components/TextForm.js
@@ -6,18 +6,24 @@ const handleUpClick = () => {
         let newText = text.toUpperCase();
         setText(newText);
     }
-  const handleLoClick = () => {
+const handleLoClick = () => {
          console.log("Lowercase was Clicked" + text);
          let newText = text.toLowerCase();
          setText(newText);
 
     }
 
+const handleClearClick = (event) => {
+        console.log("On change");
+        setText(event.target.value);
+    }
+
+
 const handleOnChange = (event) => {
         console.log("On change");
         setText(event.target.value);
     }
-const [text, setText] = useState('Enter text here');
+const [text, setText] = useState('');
  return (
     <>
     <div>
@@ -26,8 +32,9 @@ const [text, setText] = useState('Enter text here');
     <div className="mb-3">
     <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
     </div>
-    <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
-    <button className="btn btn-primary" onClick={handleLoClick}>Convert to Lowercase</button>
+    <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
+    <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
+    <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
     </div>
 
     <div className="container my-3">
